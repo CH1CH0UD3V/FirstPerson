57f76fc1997e86f91e0a650e9be7ee50ab55022d
Chichou <ch1ch0ud3v@gmail.com>
2022-09-19 16:33:24 +0200

    creation sound, Animation and GameObject

--- Assets/Interaction.cs
+++ Assets/Interaction.cs
@@ -4,16 +4,15 @@ using UnityEngine;
 
 public class Interaction : MonoBehaviour
 {
-    [SerializeField] List<Target> target;
     void Update()
     {
-        Debug.DrawRay(transform.position, transform.forward, Color.red);
+        Debug.DrawRay(transform.position, transform.forward, Color.red, 10f);
 
         //var myLayerMask = LayerMask.GetMask ("Joueur", "Default");
 
-        if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hitInfo, 2f))
+        if (Physics.Raycast (transform.position, transform.forward, out RaycastHit hitInfo, 10f))
         {
-            hitInfo = Target;
+            Debug.Log ($"j'ai touché quelqu'un qui ne sait pas qu'il va creuver, il s'appelle {hitInfo.collider.name}");
         }
     }
 }
